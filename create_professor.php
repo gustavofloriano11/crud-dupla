2f83162bd59e819f94d213cc8a062f5d0d91ba12
gustavofloriano11 <gustavo_floriano1@estudante.sesisenai.org.br>
2024-09-23 09:28:41 -0300

    create do professores

---
+++ create_professor.php
@@ -0,0 +1,41 @@
+<?php
+    include 'db.php';
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $telefone = $_POST['telefone'];
+        $salario = $_POST['salario'];
+
+        $sql = "INSERT INTO professores (nome, email, telefone, salario) VALUES ('$nome', '$email', '$telefone', '$salario')";
+
+        if($conn -> query($sql) === TRUE){
+           echo 'NOVO PROFESSOR ADICIONADO'; 
+        } else {
+            echo 'ERRO' . $sql . "<br>" . $conn -> error;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inserir Dados</title>
+</head>
+<body>
+    <h2>INSERIR DADOS DO PROFESSOR:</h2>
+    <form method="POST">
+        <label for="nome">Professor(a):</label>
+        <input type="text" name="nome">
+        <label for="email">Email:</label>
+        <input type="email" name="email">
+        <label for="telefone">Telefone:</label>
+        <input type="text" name="telefone" placeholder="47 9 99999999">
+        <label for="salario">Salário:</label>
+        <input type="text" name="salario">
+        <input type="submit" value="ENVIAR DADOS">
+    </form>
+</body>
+</html>
