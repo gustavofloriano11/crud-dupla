61f1bcb8cbcff5096ed9c6ae7660ca14845b198c
Otavio101 <otavio_roecker@estudante.sesisenai.org.br>
2024-09-23 09:31:14 -0300

    read criado

---
+++ read.php
@@ -0,0 +1,41 @@
+<?php
+    include 'db.php';
+
+
+$sql = "SELECT * FROM aulas";
+
+$result = $conn -> query($sql);
+
+if($result -> num_rows > 0){
+    echo "<table border='1'>
+        <tr>
+            <th> ID </th>
+            <th> Nome </th>
+            <th> Email </th>
+            <th> Telefone </th>
+            <th> Salário </th>
+            <th> Ações </th>
+        </tr>";
+        while($row = $result -> fetch_assoc()){
+            echo "<tr>
+                    <td> {$row['id_professor']} </td>
+                    <td> {$row['nome']} </td>
+                    <td> {$row['email']} </td>
+                    <td> {$row['telefone']} </td>
+                    <td> {$row['salario']} </td>
+                    <td>
+                        <a href='update.php?id={$row['id']}'>Editar</a> |
+                        <a href='delete.php?id={$row['id']}'>Excluir</a>
+                    </td>
+                </tr>";
+        }
+    echo "</table>";
+}else{
+    echo "Nenhum registro encontrado.";
+}
+$conn -> close();
+
+?>
+<br>
+<a href="create_professor.php">Inserir novo registro de professor.</a> <br>
+<a href="create_aula.php">Inserir novo registro de aula.</a>
